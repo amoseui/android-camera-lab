3be2ec5d11f2925cb7c9b65e56a5ff4fc15b9dd9
Euisang Lim <amoseui@gmail.com>
2021-07-30 07:53:58 +0900

    Create project with CI (#5)
    * Create project by Android Studio: Fragment + ViewModel
    * Github Actions + jacoco
    * jacocoTestReportDebug
    * detekt

---
+++ app/src/test/java/com/amoseui/cameraplayground/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package com.amoseui.cameraplayground
+
+import com.amoseui.cameraplayground.ui.main.sampleMethod
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, sampleMethod(2, 2))
+    }
+}
