8402e7049230cbb7622081e89af63d4cba0a7456
Amos Lim <amoseui@gmail.com>
2022-03-14 23:20:47 +0900

    ktlint

--- app/src/test/java/com/amoseui/cameraplayground/ExampleUnitTest.kt
+++ app/src/test/java/com/amoseui/cameraplayground/ExampleUnitTest.kt
@@ -1,9 +1,8 @@
 package com.amoseui.cameraplayground
 
 import com.amoseui.cameraplayground.ui.main.sampleMethod
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
