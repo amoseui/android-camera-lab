0061e0be18057996f9eb72cd78f1669fdfa564e4
Amos Lim <amoseui@gmail.com>
2023-08-19 19:36:06 +0900

    Migrate gradle from Groovy to Kotlin DNS (#30)
    Issue: #28

---
+++ app/build.gradle.kts
@@ -0,0 +1,71 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+
+    alias(libs.plugins.io.gitlab.arturbosch.detekt)
+    alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
+}
+
+android {
+    namespace = "com.amoseui.cameralab"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.amoseui.cameralab"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 10
+        versionName = "0.1.0-dev"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        debug {
+            enableUnitTestCoverage = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.core.ktx)
+    implementation(libs.material)
+
+    implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+
+    implementation(libs.fragment.ktx)
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
+
+detekt {
+    toolVersion = "1.18.1"
+    config = files("config/detekt/detekt.yml")
+    allRules = true
+    buildUponDefaultConfig = false
+    reports {
+        html {
+            enabled = true
+        }
+    }
+}
