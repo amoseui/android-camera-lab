38eec5c99fd76a1d00f6bf4052605cbcc207217a
Amos Lim <amoseui@gmail.com>
2023-09-04 21:56:41 +0900

    camera1

--- app/src/main/java/com/amoseui/cameralab/ui/main/Camera1.kt
+++ app/src/main/java/com/amoseui/cameralab/ui/main/Camera1.kt
@@ -11,7 +11,13 @@ import java.lang.RuntimeException
 
 class Camera1 : ICamera {
 
-    private val TAG = "Camera1"
+    private companion object {
+        private const val TAG = "Camera1"
+
+        // TODO(eui-sang.lim): temporarily fixed preview size
+        private const val previewWidth = 1920
+        private const val previewHeight = 1280
+    }
 
     private var camera: Camera? = null
 
@@ -32,67 +38,93 @@ class Camera1 : ICamera {
     }
 
     private fun stopPreviewAndCamera() {
-        preview?.setCamera(null)
+        // preview?.setCamera(null)
         camera?.also {
             it.release()
             camera = null
         }
     }
-}
-class Preview(
-    context: Context,
-    val surfaceView: SurfaceView = SurfaceView(context)
-) : ViewGroup(context), SurfaceHolder.Callback {
 
-    private var mCamera : Camera? = null
+    inner class Preview(
+        context: Context,
+        private val surfaceView: SurfaceView = SurfaceView(context)
+    ) : ViewGroup(context), SurfaceHolder.Callback {
 
-    var mHolder: SurfaceHolder = surfaceView.holder.apply {
-        addCallback(this@Preview)
-        setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
-    }
+        private var mCamera : Camera? = null
 
-    fun setCamera(camera: Camera?) {
-        if (mCamera == camera) {
-            return
+        private var holder: SurfaceHolder = surfaceView.holder.apply {
+            addCallback(this@Preview)
+            setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
         }
 
-        stopPreviewAndFreeCamera()
-
-        mCamera = camera
-
-        mCamera?.apply {
-
-            try {
-                setPreviewDisplay(holder)
-            } catch (e: IOException) {
-                e.printStackTrace()
+        fun setCamera(camera: Camera?) {
+            if (mCamera == camera) {
+                return
             }
 
-            // Important: Call startPreview() to start updating the preview
-            // surface. Preview must be started before you can take a picture.
-            startPreview()
+            stopPreviewAndFreeCamera()
+
+            mCamera = camera
+
+            mCamera?.apply {
+
+                try {
+                    setPreviewDisplay(holder)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                // Important: Call startPreview() to start updating the preview
+                // surface. Preview must be started before you can take a picture.
+                startPreview()
+            }
         }
-    }
-    override fun surfaceDestroyed(holder: SurfaceHolder) {
-        // Surface will be destroyed when we return, so stop the preview.
-        // Call stopPreview() to stop updating the preview surface.
-        mCamera?.stopPreview()
-    }
 
-    /**
-     * When this function returns, mCamera will be null.
-     */
-    private fun stopPreviewAndFreeCamera() {
-        mCamera?.apply {
+        override fun surfaceCreated(p0: SurfaceHolder) {
+            TODO("Not yet implemented")
+        }
+
+        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+            mCamera?.apply {
+                // Now that the size is known, set up the camera parameters and begin
+                // the preview.
+                parameters?.also { params ->
+                    params.setPreviewSize(previewWidth, previewHeight)
+                    requestLayout()
+                    parameters = params
+                }
+
+                // Important: Call startPreview() to start updating the preview surface.
+                // Preview must be started before you can take a picture.
+                startPreview()
+            }
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder) {
+            // Surface will be destroyed when we return, so stop the preview.
             // Call stopPreview() to stop updating the preview surface.
-            stopPreview()
+            mCamera?.stopPreview()
+        }
 
-            // Important: Call release() to release the camera for use by other
-            // applications. Applications should release the camera immediately
-            // during onPause() and re-open() it during onResume()).
-            release()
+        /**
+         * When this function returns, mCamera will be null.
+         */
+        private fun stopPreviewAndFreeCamera() {
+            mCamera?.apply {
+                // Call stopPreview() to stop updating the preview surface.
+                stopPreview()
 
-            mCamera = null
+                // Important: Call release() to release the camera for use by other
+                // applications. Applications should release the camera immediately
+                // during onPause() and re-open() it during onResume()).
+                release()
+
+                mCamera = null
+            }
+        }
+
+        override fun onLayout(p0: Boolean, p1: Int, p2: Int, p3: Int, p4: Int) {
+            TODO("Not yet implemented")
         }
     }
 }
