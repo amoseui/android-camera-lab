6addf4f2ac74356e35678031ed05ff3bf00c6678
Amos Lim <amoseui@gmail.com>
2024-01-06 13:29:43 +0900

    spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,33 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.spotless)
 }
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        target("**/*.kt")
+        targetExclude("$buildDir/**/*.kt")
+        ktlint()
+        licenseHeaderFile(rootProject.file("spotless/spotless.license.kt"))
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    kotlinGradle {
+        target("**/*.kts")
+        targetExclude("$buildDir/**/*.kts")
+        ktlint()
+        licenseHeaderFile(rootProject.file("spotless/spotless.license.kt"))
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    format("xml") {
+        target("**/*.xml")
+        targetExclude("**/build/**/*.xml")
+        licenseHeaderFile(rootProject.file("spotless/spotless.license.xml"), "(<[^!?])")
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
+
 true // Needed to make the Suppress annotation work for the plugins block
