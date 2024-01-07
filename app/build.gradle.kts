9f375a7739565034cb50cd105082075fd5ce2195
Amos Lim <amoseui@gmail.com>
2024-01-07 16:36:13 +0900

    Apply spotless and update gradle (#32)
    Issue: https://github.com/amoseui/android-camera-lab/issues/31
    
    following setup from
    https://github.com/skydoves/Pokedex/blob/main/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2024 amoseui (Amos Lim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
 
     alias(libs.plugins.io.gitlab.arturbosch.detekt)
     alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
@@ -24,18 +40,25 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
         }
         debug {
             enableUnitTestCoverage = true
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
--- spotless/spotless.license.kt
+++ spotless/spotless.license.kt
@@ -14,25 +14,3 @@
  * limitations under the License.
  */
 
-package com.amoseui.cameralab.ui.main
-
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.amoseui.cameralab.R
-
-class SettingsFragment : Fragment() {
-    companion object {
-        fun newInstance() = SettingsFragment()
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
-    }
-}
